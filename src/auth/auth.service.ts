179a7205ff04a9aa28b9260c61772137402e3f31
Shravani <shravaniaparab@gmail.com>
2026-01-21 18:41:38 +0530

    Initial commit

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../prisma/prisma.service';
+import * as bcrypt from 'bcrypt';
+
+@Injectable()
+export class AuthService {
+  constructor(private prisma: PrismaService) {}
+
+  async register(dto: any) {
+    const hashedPassword = await bcrypt.hash(dto.password, 10);
+
+    return this.prisma.user.create({
+      data: {
+        name: dto.name,
+        email: dto.email,
+        password: hashedPassword,
+        role: dto.role,
+        walletAddress: dto.walletAddress,
+      },
+    });
+  }
+}
